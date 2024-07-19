23abfef60416d33ed5f48ae1820bb6f9ad9ce44a
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-19 13:10:45 +0100

    refactor(HNG-5): adjust indentation

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,7 +1,11 @@
 export default function Logo({ expanded }: { expanded: boolean }) {
   return (
     <h1
-      className={`text-primary-foreground uppercase font-[500] ${expanded ? 'text-[1.25rem] leading-[1.2rem]' : 'text-[0.75rem] leading-normal'}`}
+      className={`text-primary-foreground uppercase font-[500] ${
+        expanded
+          ? "text-[1.25rem] leading-[1.2rem]"
+          : "text-[0.75rem] leading-normal"
+      }`}
     >
       logo
     </h1>
