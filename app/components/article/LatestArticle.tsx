ad70731e34475a06e27eb4375317ffa88adcd336
obanijesuadufe8 <obanijesuadufe8@gmail.com>
2024-07-19 17:07:57 +0000

    fix: fix the image overflow and make the article card into a link

--- app/components/article/LatestArticle.tsx
+++ app/components/article/LatestArticle.tsx
@@ -1,3 +1,5 @@
+import { Link } from "@remix-run/react";
+
 interface props {
     title: string;
     description: string;
@@ -12,47 +14,49 @@ interface props {
 
 export default function LatestArticle({ article }: { article: props }) {
     return (
-        <article className="text-[#525252] max-w-[1000px] bg-[#fafafa]">
-            <div className="grid md:grid-cols-2 gap-[24px] p-[24px] md:border-b-[1px] border-[#525252]">
-                <div className="space-y-[16px] md:px-[8px] lg:p-[16px] p-[16px]">
-                    <span className="bg-[#F97316] pl-[10px] pr-[12px] py-[4px] rounded-full inline-flex justify-center items-center gap-[6px] text-[12px] font-[700]">
-                        <span className="h-[8px] w-[8px] rounded-full bg-black"></span>
-                        <div className="uppercase">
-                            {article.tag}
-                        </div>
-                    </span>
+        <Link to={article.link}>
+            <article className="text-[#525252] max-w-[1000px] bg-[#fafafa]">
+                <div className="grid md:grid-cols-2 gap-[24px] p-[24px] md:border-b-[1px] border-[#525252]">
+                    <div className="space-y-[16px] md:px-[8px] lg:p-[16px] p-[16px]">
+                        <span className="bg-[#F97316] pl-[10px] pr-[12px] py-[4px] rounded-full inline-flex justify-center items-center gap-[6px] text-[12px] font-[700]">
+                            <span className="h-[8px] w-[8px] rounded-full bg-black"></span>
+                            <div className="uppercase">
+                                {article.tag}
+                            </div>
+                        </span>
 
-                    <h3 className="md:text-[28px] text-[20px] md:font-[600] font-[700] capitalize leading-[normal]">
-                        {article.title}
-                    </h3>
+                        <h3 className="md:text-[28px] text-[20px] md:font-[600] font-[700] capitalize leading-[normal]">
+                            {article.title}
+                        </h3>
 
-                    <p className="font-[400] text-[14px] md:text-[18px] leading-[normal]">
-                        {article.description}
-                    </p>
+                        <p className="font-[400] text-[14px] md:text-[18px] leading-[normal]">
+                            {article.description}
+                        </p>
 
-                    <div className="flex justify-between items-center md:font-[500] font-[400] text-[14px] md:text-[16px]">
-                        <div className="flex items-center gap-[8px]">
-                            <img src={article.profileImage} alt={article.name} className="rounded-full w-[24px] md:w-[40px] h-[24px] md:h-[40px] object-cover object-top" />
-                            <p className="font-[500] text-center">
-                                {article.name}
+                        <div className="flex justify-between items-center md:font-[500] font-[400] text-[14px] md:text-[16px]">
+                            <div className="flex items-center gap-[8px]">
+                                <img src={article.profileImage} alt={article.name} className="rounded-full w-[24px] md:w-[40px] h-[24px] md:h-[40px] object-cover object-top" />
+                                <p className="font-[500] text-center">
+                                    {article.name}
+                                </p>
+                            </div>
+                            <p className="text-center">
+                                {article.time}
+                                {" "}
+                                Read
+                            </p>
+
+                            <p className="text-center">
+                                {article.creationDate}
                             </p>
                         </div>
-                        <p className="text-center">
-                            {article.time}
-                            {" "}
-                            Read
-                        </p>
 
-                        <p className="text-center">
-                            {article.creationDate}
-                        </p>
                     </div>
-
+                    <div className="flex items-center">
+                        <img src={article.image} alt={article.title} className="w-full md:h-[300px] h-[250px] object-cover rounded-[6px] md:rounded-[8px] border-b-[1px] md:border-0 border-[#525252]" />
+                    </div>
                 </div>
-                <div className="flex items-stretch">
-                    <img src={article.image} alt={article.title} className="w-full h-full object-cover rounded-[6px] md:rounded-[8px] border-b-[1px] md:border-0 border-[#525252]" />
-                </div>
-            </div>
-        </article>
+            </article>
+        </Link>
     )
 }
