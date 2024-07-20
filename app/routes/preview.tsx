38427775c0bc3ad9b1458d890183295602216e27
kehinde_Daniels <kehindedaniels501@gmail.com>
2024-07-20 14:23:26 +0100

    feat: implement floating footer cookie consent component

---
+++ app/routes/preview.tsx
@@ -0,0 +1,10 @@
+import FooterCookieConsent from "~/components/ui/footerCookieConsent";
+
+export default function Test() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Test FooterCookieConsent Component</h1>
+      <FooterCookieConsent />
+    </div>
+  );
+}
