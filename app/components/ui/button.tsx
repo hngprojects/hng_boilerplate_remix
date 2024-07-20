64bf0dba29b4d9e092fd608f261ed1f4dd7ba47d
kehinde_Daniels <kehindedaniels501@gmail.com>
2024-07-20 12:49:34 +0100

    feat: implement cookie consent banner with custom button styles

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,56 +1,45 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
-
-import { cn } from "app/lib/utils/cn"
-
-const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
-  {
-    variants: {
-      variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
-      },
-      size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
-    },
-  }
-)
-
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick: () => void;
+  variant?: "primary" | "secondary" | "destructive";
+  className?: string;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Button.displayName = "Button"
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  variant = "primary",
+  className = "",
+}) => {
+  const baseStyles =
+    "w-40 px-4 py-2 rounded text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 transition-colors";
+  let variantStyles = "";
 
-export { Button, buttonVariants }
+  switch (variant) {
+    case "primary":
+      variantStyles =
+        "bg-destructive text-destructive-foreground  focus:ring-destructive border hover:border-destructive hover:bg-destructive-foreground hover:text-destructive focus:ring-destructive";
+      break;
+    case "secondary":
+      variantStyles =
+        "bg-secondary text-destructive border border-destructive hover:bg-destructive-foreground hover:text-destructive focus:ring-destructive";
+      break;
+    case "destructive":
+      variantStyles =
+        "bg-destructive text-destructive-foreground  focus:ring-destructive border hover:border-destructive hover:bg-destructive-foreground hover:text-destructive focus:ring-destructive";
+      break;
+    default:
+      variantStyles =
+        "bg-primary text-primary-foreground hover:bg-primary-foreground hover:text-primary focus:ring-primary";
+  }
+
+  const buttonClasses = `${baseStyles} ${variantStyles} ${className}`;
+
+  return (
+    <button className={buttonClasses} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
