d9e733579b3a7cee5c73198b0942d3641d0347fe
Wealth <shuaibganiyat20@gmail.com>
2024-07-20 08:42:34 +0100

    feat(HNG-2145):create user dashboard product details component

--- app/routes/products.tsx
+++ app/routes/products.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import ProductDetails from '../components/ProductDetails';
+import ProdImg from '../../public/Frame1321315415.png';
 
 interface Product {
-  id: number;
+  id: string;
   name: string;
   imageUrl: string;
   category: string;
@@ -14,9 +15,9 @@ interface Product {
 
 const products: Product[] = [
   {
-    id: 1,
-    name: 'P001',
-    imageUrl: 'https://via.placeholder.com/150',
+    id: 'P001',
+    name: 'Product 1',
+    imageUrl: ProdImg,
     category: 'Breakfast',
     dateAdded: '2024-06-06, 12:34:56',
     stock: 10,
@@ -24,9 +25,9 @@ const products: Product[] = [
     description: 'A fusion of ripe bananas, pure honey, and succulent raspberries with our bread. Crafted to perfection smiles.',
   },
   {
-    id: 2,
-    name: 'P002',
-    imageUrl: 'https://via.placeholder.com/150',
+    id: 'P002',
+    name: 'Product 2',
+    imageUrl: ProdImg,
     category: 'Appetizers',
     dateAdded: '2024-06-06, 12:34:56',
     stock: 20,
@@ -35,10 +36,10 @@ const products: Product[] = [
   },
 ];
 
-const ProductsPage = () => {
+const ProductsPage: React.FC = () => {
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
 
-  const handleProductClick = (productId: number) => {
+  const handleProductClick = (productId: string) => {
     const product = products.find(p => p.id === productId) || null;
     setSelectedProduct(product);
   };
@@ -46,6 +47,7 @@ const ProductsPage = () => {
   const handleClose = () => {
     setSelectedProduct(null);
   };
+
   const handleEdit = (productId: string) => {
     console.log(`Edit product with ID: ${productId}`);
     // Implement the Edit functionality here
@@ -56,24 +58,30 @@ const ProductsPage = () => {
     // Implement the Delete functionality here
   };
 
-
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Products</h1>
       <ul className="space-y-2">
         {products.map(product => (
           <li key={product.id}>
-            <button onClick={() => handleProductClick(product.id)} className="text-blue-500 hover:underline">
+            <button
+              onClick={() => handleProductClick(product.id)}
+              className="text-blue-500 hover:underline"
+            >
               {product.name}
             </button>
           </li>
         ))}
       </ul>
 
-      {selectedProduct && <ProductDetails 
-      product={selectedProduct} 
-      onClose={handleClose} onEdit={handleEdit}
-      onDelete={handleDelete}/>}
+      {selectedProduct && (
+        <ProductDetails
+          product={selectedProduct}
+          onClose={handleClose}
+          onEdit={handleEdit}
+          onDelete={handleDelete}
+        />
+      )}
     </div>
   );
 };
