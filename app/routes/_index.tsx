264d760577e84b6058442d82e3841d6682dad6ca
corybantes <farukadedeji17@gmail.com>
2024-07-20 14:43:35 -0700

    feat: create accordion component

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,60 +1,9 @@
-import type { MetaFunction } from "@remix-run/node";
-
-import { Button } from "~/components/ui/button";
-import CardPlatform from "~/components/ui/card/card-platform";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+import { AccordionComponent } from "~/components/accordionComponent";
 
 export default function Index() {
   return (
-    <div className="p-4 font-sans">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="mt-4 list-disc space-y-2 pl-6">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <Button>Hello</Button>
-        <div className="p-2">
-          <CardPlatform
-            logo="/images/g-drive-icon.svg"
-            heading="Drive"
-            description="Store, share, and collaborate on documents and files securely"
-            containerClassName="max-w-[341px]"
-          />
-        </div>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className=" ">
+      <AccordionComponent />
     </div>
   );
 }
