f0ead9b6e1297940f305b145f174a13d57c3e961
Oluwaseyi Tommy <130176232+seshconcept@users.noreply.github.com>
2024-07-20 11:40:36 +0100

    feat/HNG-16-Implement-the-login-page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,6 +12,7 @@ export default function Index() {
   return (
     <div className="font-sans p-4">
       <h1 className="text-3xl">Welcome to Remix</h1>
+
       <ul className="list-disc mt-4 pl-6 space-y-2">
         <li>
           <a
