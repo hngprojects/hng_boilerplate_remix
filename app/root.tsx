25eee4cf1846873d69ee93954a844e432f49c41f
Frankpesta <franklinolisaemeka3@gmail.com>
2024-07-20 13:51:47 +0100

    feat: created footer component for static external pages

--- app/root.tsx
+++ app/root.tsx
@@ -1,43 +1,45 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
 import { LinksFunction } from "@remix-run/node";
 import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
 } from "@remix-run/react";
 import type { ReactNode } from "react";
 
 import { AdminSideNavBar } from "./components/SuperAdminSideBar/SuperAdminSideNavBar";
 import styles from "./styles/global.css?url";
+import Footer from "./components/ui/Footer";
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: styles },
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+	{ rel: "stylesheet", href: styles },
+	...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
 export function Layout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
         <div className="flex">
           <AdminSideNavBar />
-          <main className="flex-1">{children}</main>,
-          <ScrollRestoration />
-          <Scripts />
+  				<main className="flex-1">{children}</main>,
+  				<Footer />
+				<ScrollRestoration />
+  				<Scripts />
         </div>
-      </body>
-    </html>
-  );
+			</body>
+		</html>
+	);
 }
 
 export default function App() {
-  return <Outlet />;
+	return <Outlet />;
 }
