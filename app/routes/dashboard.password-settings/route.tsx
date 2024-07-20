aa6d984777243c792acb8fc6c8850cd53d1b5bb6
lftobs <tobbytobs1@gmail.com>
2024-07-20 17:16:19 +0100

    feat(116):user-dashboard-password-settings (completed password update functionality)

--- app/routes/dashboard.password-settings/route.tsx
+++ app/routes/dashboard.password-settings/route.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import PasswordUpdate from '~/components/dashboard/PasswordUpdate'
+import PasswordUpdate from '~/components/dashboard/PasswordUpdate'
 
 const DaashboardPasswordSettings = () => {
   return (
@@ -7,7 +7,7 @@ const DaashboardPasswordSettings = () => {
         <div className="h-16  grid place-items-center font-extrabold">Nav</div>
         <div className="flex">
             <div className="w-[20%] h-svh bg-[rgba(203,213,225,.3)] grid place-items-center font-extrabold max-lg:hidden">Sidebar</div>
-            {/* <PasswordUpdate /> */}
+            <PasswordUpdate />
       </div>
     </>
   )
