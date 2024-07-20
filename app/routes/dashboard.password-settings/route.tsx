ae70e8d065d011592a07fcc8680eabb752d17515
lftobs <tobbytobs1@gmail.com>
2024-07-20 23:11:25 +0100

    feat(116):user-dashboard-password-settings (completed password update functionality-finalv0.5:fixed-lint-issues)

--- app/routes/dashboard.password-settings/route.tsx
+++ app/routes/dashboard.password-settings/route.tsx
@@ -1,15 +1,17 @@
-import PasswordUpdate from '~/components/dashboard/PasswordUpdate'
+import PasswordUpdate from "~/components/dashboard/PasswordUpdate";
 
 const DaashboardPasswordSettings = () => {
   return (
     <>
-        <div className="h-16  grid place-items-center font-extrabold">Nav</div>
-        <div className="flex">
-            <div className="w-[20%] h-svh bg-[rgba(203,213,225,.3)] grid place-items-center font-extrabold max-lg:hidden">Sidebar</div>
-            <PasswordUpdate />
+      <div className="grid h-16 place-items-center font-extrabold">Nav</div>
+      <div className="flex">
+        <div className="grid h-svh w-[20%] place-items-center bg-[rgba(203,213,225,.3)] font-extrabold max-lg:hidden">
+          Sidebar
+        </div>
+        <PasswordUpdate />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default DaashboardPasswordSettings
+export default DaashboardPasswordSettings;
