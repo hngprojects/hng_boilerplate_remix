157cfa77273bda8cb18978ec1e291b87a828ae15
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2024-07-20 20:25:10 +0200

    feat: Implement Create role modal

--- app/components/ui/create-role-modal.tsx
+++ app/components/ui/create-role-modal.tsx
@@ -7,7 +7,10 @@ export type ModalsType = {
   onClose?: () => void;
 };
 
-const CreateRoleModal: FunctionComponent<ModalsType> = ({ className = "", onClose }) => {
+const CreateRoleModal: FunctionComponent<ModalsType> = ({
+  className = "", 
+  onClose 
+}) => {
   return (
     <div
       role="dialog"
@@ -29,12 +32,20 @@ const CreateRoleModal: FunctionComponent<ModalsType> = ({ className = "", onClos
           aria-label="Close"
           className="relative h-6 w-6 cursor-pointer overflow-hidden"
         >
-          <img className="h-full w-full" loading="lazy" alt="Close" src="/closeicon.png" />
+          <img 
+          className="h-full w-full" 
+          loading="lazy" 
+          alt="Close" 
+          src="/closeicon.png" 
+          />
         </div>
       </div>
       <Form action="" method="post" className="flex flex-col gap-6">
         <div className="flex flex-col items-start gap-2">
-          <label htmlFor="roleName" className="text-base font-semibold text-gray-900">
+          <label 
+          htmlFor="roleName" 
+          className="text-base font-semibold text-gray-900"
+          >
             Name of role
           </label>
           <input
@@ -47,7 +58,9 @@ const CreateRoleModal: FunctionComponent<ModalsType> = ({ className = "", onClos
           />
         </div>
         <div className="flex flex-col items-start gap-2">
-          <label htmlFor="roleDescription" className="text-base font-semibold text-gray-900">
+          <label htmlFor="roleDescription" 
+          className="text-base font-semibold text-gray-900"
+          >
             Role description
           </label>
           <textarea
