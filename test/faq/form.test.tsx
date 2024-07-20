d2e1e4b56043127c010a94d1c74071397f81fc76
Abdulmalik Yusuf <31839174+abdulmalikyusuf@users.noreply.github.com>
2024-07-20 06:32:36 +0100

    fix: added one more test

--- test/faq/form.test.tsx
+++ test/faq/form.test.tsx
@@ -1,16 +1,24 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
+import RTLUserEvent from "@testing-library/user-event";
 import { describe, expect, it } from "vitest";
 import "@testing-library/jest-dom";
 import QuestionForm from "~/components/faq/form";
 
+function setup(jsx: JSX.Element) {
+  return {
+    user: RTLUserEvent.setup(),
+    ...render(jsx),
+  };
+}
+
 describe("FAQ Question Form", async () => {
-  beforeEach(() => {
-    render(<QuestionForm />);
-  });
-  // afterEach(()=>)
+  beforeEach(() => {});
+
+  afterEach(() => cleanup());
 
   describe("renders inputs and button correctly", () => {
     it("form is rendered", () => {
+      render(<QuestionForm />);
       expect(screen.getByTestId("header")).toBeInTheDocument();
       expect(screen.getByTestId("description")).toBeInTheDocument();
       expect(screen.getByTestId("name")).toBeInTheDocument();
@@ -20,43 +28,45 @@ describe("FAQ Question Form", async () => {
       expect(screen.getByTestId("submit-button")).toBeInTheDocument();
     });
 
-    it("fields are accepting inputs", () => {
-      // Fill out the form fields
-      fireEvent.change(screen.getByTestId("name"), {
-        target: { value: "John Doe" },
-      });
-      fireEvent.change(screen.getByTestId("email"), {
-        target: { value: "yusuffabdulmalik@gmail.com" },
-      });
-      fireEvent.change(screen.getByTestId("phoneNumber"), {
-        target: { value: "08147359155" },
-      });
-      fireEvent.change(screen.getByTestId("message"), {
-        target: { value: "This is a message." },
-      });
-      expect(screen.getByTestId("name")).toHaveValue("John Doe");
-      expect(screen.getByTestId("email")).toHaveValue(
-        "yusuffabdulmalik@gmail.com"
-      );
-      expect(screen.getByTestId("phoneNumber")).toHaveValue("08147359155");
-      expect(screen.getByTestId("message")).toHaveValue("This is a message.");
+    it("fields are accepting inputs", async () => {
+      const { user, getByTestId } = setup(<QuestionForm />);
+      await user.type(getByTestId("name"), "John Doe");
+      await user.type(getByTestId("email"), "yusuffabdulmalik@gmail.com");
+      await user.type(getByTestId("phoneNumber"), "08147359155");
+      await user.type(getByTestId("message"), "This is a message.");
+
+      expect(getByTestId("name")).toHaveValue("John Doe");
+      expect(getByTestId("email")).toHaveValue("yusuffabdulmalik@gmail.com");
+      expect(getByTestId("phoneNumber")).toHaveValue("08147359155");
+      expect(getByTestId("message")).toHaveValue("This is a message.");
     });
-    it("button is not disabled and is clickable when inputs are entered correctly", async () => {
-      fireEvent.change(screen.getByTestId("name"), {
-        target: { value: "John Doe" },
-      });
-      fireEvent.change(screen.getByTestId("email"), {
-        target: { value: "yusuffabdulmalik@gmail.com" },
-      });
-      fireEvent.change(screen.getByTestId("phoneNumber"), {
-        target: { value: "08147359155" },
-      });
-      fireEvent.change(screen.getByTestId("message"), {
-        target: { value: "This is a message." },
-      });
-      await waitFor(() => {
-        expect(screen.getByTestId("submit-button")).not.toBeDisabled();
-      });
+
+    it("button is not disabled and is clickable on valid inputs", async () => {
+      const { user, getByTestId } = setup(<QuestionForm />);
+      // Fill out the form fields
+      await user.type(getByTestId("name"), "John Doe");
+      await user.type(getByTestId("email"), "yusuffabdulmalik@gmail.com");
+      await user.type(getByTestId("phoneNumber"), "08147359155");
+      await user.type(getByTestId("message"), "This is a message.");
+
+      expect(getByTestId("submit-button")).not.toBeDisabled();
+    });
+  });
+  describe("on submission", () => {
+    it("clear fields on success", async () => {
+      const { user, getByTestId } = setup(<QuestionForm />);
+
+      await user.type(getByTestId("name"), "John Doe");
+      await user.type(getByTestId("email"), "yusuffabdulmalik@gmail.com");
+      await user.type(getByTestId("phoneNumber"), "08147359155");
+      await user.type(getByTestId("message"), "This is a message.");
+
+      await user.click(getByTestId("submit-button"));
+
+      expect(getByTestId("name")).toHaveValue("");
+      expect(getByTestId("email")).toHaveValue("");
+      expect(getByTestId("phoneNumber")).toHaveValue("");
+      expect(getByTestId("message")).toHaveValue("");
     });
   });
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,11 @@
 // vitest.config.ts
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
   },
+  plugins: [tsconfigPaths()],
 });
