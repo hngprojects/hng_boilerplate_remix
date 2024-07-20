d358e053d2089fb1c718519f6b13afe004a1b63e
Frankpesta <franklinolisaemeka3@gmail.com>
2024-07-20 13:51:47 +0100

    feat: created footer component for static external pages

--- app/root.tsx
+++ app/root.tsx
@@ -13,30 +13,26 @@ import { cssBundleHref } from "@remix-run/css-bundle";
 import Footer from "./components/ui/Footer";
 
 export const links: LinksFunction = () => [
-	{ rel: "stylesheet", href: styles },
-	...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: styles },
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
-export function Layout({ children }: { children: ReactNode }) {
-	return (
-		<html lang="en">
-			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<Meta />
-				<Links />
-			</head>
-			<body>
-        <div className="flex">
-          <AdminSideNavBar />
-  				<main className="flex-1">{children}</main>,
-  				<Footer />
-				<ScrollRestoration />
-  				<Scripts />
-        </div>
-			</body>
-		</html>
-	);
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
 
 export default function App() {
