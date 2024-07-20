d49a09501823c01ab7a231a3ec607010e5bb5ebb
kehinde_Daniels <kehindedaniels501@gmail.com>
2024-07-20 12:49:34 +0100

    feat: implement cookie consent banner with custom button styles

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,10 +8,9 @@ import { useNavigate } from "@remix-run/react";
 import Navbar from "~/Components/Navbar/Navbar";
 
 import { useState } from "react";
-
 import { Input } from "~/types/otpauth";
-import { Button } from "~/Components/ui/button";
 import OtpAuth from "~/Components/ui/otp/OtpAuth";
+import {Button} from "~/Components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -32,60 +31,41 @@ export default function Index() {
   const navigate = useNavigate();
 
   return (
-    <>
-    <Navbar />
-      <div className="flex gap-6">
-        <div className="max-w-52 bg-graystyles">
-          need's someones else component for this part
-        </div>
-
-        <div className="flex flex-col gap-6 my-8">
-          <div className="rounded borderstyles max-w-fit flex">
-            <Button className="text-orange bg-graystyles text-sm text-nowrap font-medium max-w-full p-2">
-              New Templates
-            </Button>
-            <Button className="text-sm max-w-full text-nowrap p-2">
-              Manage Templates
-            </Button>
-          </div>
-          <div className="text-black">
-            <h4 className="font-bold text-2xl">Create a New Template</h4>
-            <p className=" text-base">
-              choose an option below to begin crafting your email design.
-            </p>
-          </div>
-          <div  className="flex gap-4 text-sm">
-            <ul onClick={()=>navigate('/templatewithhtml')} className="flex items-center createtemplate max-w-72 gap-3 p-3 cursor-pointer">
-              <li><img src={getImages[0].open_close.imgLocation} alt="note icon" /></li>
-              <li>
-                <h4 className="font-semibold">Generate with HTML</h4>
-                <p>create an email template by pasting your custom-coded template</p>
-              </li>
-            </ul>
-            <ul className="flex items-center createtemplate max-w-72 gap-3 p-3 cursor-pointer ">
-              <li><img src={getImages[0].note_icon.imgLocation} alt="" /></li>
-              <li>
-                <h4 className="font-semibold">Edit in-built Template</h4>
-                <p>create an email template by choosing from our custom template library</p>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      <OtpAuth
-        isModalOpen={openModal}
-        setIsModalOpen={() => setOpenModal(!openModal)}
-        inputs={[
-          { name: "input1", value: "" },
-          { name: "input1", value: "" },
-          { name: "input1", value: "" },
-          { name: "input1", value: "" },
-          { name: "input1", value: "" },
-          { name: "input1", value: "" },
-        ]}
-        handleSubmit={handleSubmit}
-      />
-
-    </>)
+  <div className="font-sans p-4">
+      <h1 className="text-3xl">Welcome to Remix</h1>
+      <ul className="list-disc mt-4 pl-6 space-y-2">
+        <li>
+          <a
+            className="text-blue-700 underline visited:text-purple-900"
+            target="_blank"
+            href="https://remix.run/start/quickstart"
+            rel="noreferrer"
+          >
+            5m Quick Start
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-blue-700 underline visited:text-purple-900"
+            target="_blank"
+            href="https://remix.run/start/tutorial"
+            rel="noreferrer"
+          >
+            30m Tutorial
+          </a>
+        </li>
+        <Button >Hello</Button>
+        <li>
+          <a
+            className="text-blue-700 underline visited:text-purple-900"
+            target="_blank"
+            href="https://remix.run/docs"
+            rel="noreferrer"
+          >
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
