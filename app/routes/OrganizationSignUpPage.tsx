7edb194d3e107e378d411dbd7991650d2231062b
Aakash kumar <76447347+itsAakashz@users.noreply.github.com>
2024-07-21 13:47:53 +0530

    Lint fix for OrganizationSignUpPage.tsx

--- app/routes/OrganizationSignUpPage.tsx
+++ app/routes/OrganizationSignUpPage.tsx
@@ -6,7 +6,6 @@ import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 
-
 interface FormData {
   companyName: string;
   companyEmail: string;
