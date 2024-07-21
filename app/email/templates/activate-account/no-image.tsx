fc278e93738b2397daf86668ab59a0cbdbdea3d8
Abdul Hakeem <abdulhakeem080255@gmail.com>
2024-07-21 16:32:56 +0100

    Email template to activate account - SOLVING PNPM LINT

--- app/email/templates/activate-account/no-image.tsx
+++ app/email/templates/activate-account/no-image.tsx
@@ -28,7 +28,7 @@ const ViewActLink = (property: propertyDataType) => {
                   {property.message}
                 </p>
                 <div className="flex items-center justify-center">
-                  <Button className="bg-default-foreground block cursor-pointer rounded-[8px] bg-[#F97316] px-10 py-2.5 text-base text-white">
+                  <Button className="block cursor-pointer rounded-[8px] bg-[#F97316] bg-default-foreground px-10 py-2.5 text-base text-white">
                     {property.cta_text}
                   </Button>
                 </div>
