a17fdf0605f8df5fda67f6db92d6bf5690dd7d6d
adelosoye-dev <adelosoyeolajumoke9@gmail.com>
2024-07-21 01:34:29 +0100

    fix: resolve merge conflict and lint errors.

--- app/components/Dialog/Dialog.tsx
+++ app/components/Dialog/Dialog.tsx
@@ -7,23 +7,23 @@ type DialogProperties = {
 };
 const Dialog = ({ headerText, description, footer }: DialogProperties) => {
   return (
-    <div className="flex flex-col p-6 gap-4 bg-white rounded-[6px] max-w-[512px] w-auto">
+    <div className="flex w-auto max-w-[512px] flex-col gap-4 rounded-[6px] bg-white p-6">
       <header className="flex flex-col gap-2">
         <h2 className="text-lg font-semibold text-primary">{headerText}</h2>
         {description && (
-          <p className="font-normal text-sm leading-[1.05rem] text-muted-foreground text-pretty">
+          <p className="text-pretty text-sm font-normal leading-[1.05rem] text-muted-foreground">
             {description}
           </p>
         )}
       </header>
-      <footer className=" flex flex-row w-full justify-end gap-2">
+      <footer className="flex w-full flex-row justify-end gap-2">
         {footer.map((item, index) => (
-          <div key={index} className={cn("font-normal text-sm text-[#0F172A]")}>
+          <div key={index} className={cn("text-sm font-normal text-[#0F172A]")}>
             {item}
           </div>
         ))}
       </footer>
     </div>
-);
+  );
 };
 export default Dialog;
