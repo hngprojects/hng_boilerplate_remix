6f02913b5ca4df1ce8fd2a2a056970b93dfe6787
Frankpesta <franklinolisaemeka3@gmail.com>
2024-07-21 12:45:29 +0100

    feat:created footer component for external static pages

--- app/root.tsx
+++ app/root.tsx
@@ -1,39 +1,40 @@
-import {
-	Links,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
-} from "@remix-run/react";
-import { LinksFunction } from "@remix-run/node";
-import styles from "./styles/global.css?url";
 import { cssBundleHref } from "@remix-run/css-bundle";
+import { LinksFunction } from "@remix-run/node";
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
 import Footer from "./components/ui/Footer";
+import styles from "./styles/global.css?url";
 
 export const links: LinksFunction = () => [
-	{ rel: "stylesheet", href: styles },
-	...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: styles },
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang="en">
-			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<Meta />
-				<Links />
-			</head>
-			<body>
-				{children}
-				<Footer />
-				<ScrollRestoration />
-				<Scripts />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <Footer />
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
 
 export default function App() {
-	return <Outlet />;
+  return <Outlet />;
 }
