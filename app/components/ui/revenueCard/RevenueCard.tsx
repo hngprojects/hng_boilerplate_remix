cc7d22ddeeaf9e1e0fbd187aee16d20993d22149
DESKTOP-14E8GN0\JAMICO <jamicojerahmeel@gmail.com>
2024-07-21 15:47:07 +0100

    HNG-58-super-admin-product-list-page

--- app/components/ui/revenueCard/RevenueCard.tsx
+++ app/components/ui/revenueCard/RevenueCard.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import { Card, CardContent, CardHeader, CardTitle, CardDescription } from './cardComponent';
- 
+import React from "react";
+import { Card, CardContent, CardDescription } from "./cardComponent";
 
-interface RevenueCardProps {
+interface RevenueCardProperties {
   revenue: string;
   change: string;
 }
 
-const RevenueCard: React.FC<RevenueCardProps> = ({ revenue, change }) => {
+const RevenueCard: React.FC<RevenueCardProperties> = ({ revenue, change }) => {
   return (
-    <Card className=" w-full max-w-[337.33px] h-[134px] px-[24px] pt-[23px] pb-[28px]  sm:w-1/2 md:w-1/3 lg:w-1/4">
-      
+    <Card className="h-[134px] w-full max-w-[337.33px] px-[24px] pb-[28px] pt-[23px] sm:w-1/2 md:w-1/3 lg:w-1/4">
       <CardContent className=" ">
-      <div className='flex justify-between items-center pb-1'>
-      <h1 className='text-[14px] leading-[16.94px] font-medium'>Total Revenue</h1>
-     <img className='w-[24px] h-[24px]' src="/images/doller.png" alt="" />
-      </div>
-        
-          <h4 className="text-[24px] text-[#0A0A0A] font-semibold leading-[29.05px] mb-1">${revenue}</h4>
-          <CardDescription className="text-[#525252] text-[12px] leading-[14.52px] font-normal">{change}</CardDescription>
-      
+        <div className="flex items-center justify-between pb-1">
+          <h1 className="text-[14px] font-medium leading-[16.94px]">
+            Total Revenue
+          </h1>
+          <img className="h-[24px] w-[24px]" src="/images/doller.png" alt="" />
+        </div>
+
+        <h4 className="mb-1 text-[24px] font-semibold leading-[29.05px] text-[#0A0A0A]">
+          ${revenue}
+        </h4>
+        <CardDescription className="text-[12px] font-normal leading-[14.52px] text-[#525252]">
+          {change}
+        </CardDescription>
       </CardContent>
     </Card>
   );
