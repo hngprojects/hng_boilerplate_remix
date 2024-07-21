cb45abf6f6538bd10a8ff5e301ed338105724b12
KaiserX <wiz0isaac@gmail.com>
2024-07-21 18:37:29 +0100

    feat(11): implement sucessfully create role model

--- app/components/icons/CloseIcon.tsx
+++ app/components/icons/CloseIcon.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
 
-export const CloseIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-    <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-    </svg>
+export const CloseIcon: React.FC<React.SVGProps<SVGSVGElement>> = (
+  properties,
+) => (
+  <svg
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...properties}
+  >
+    {">"}
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M6 18L18 6M6 6l12 12"
+    ></path>
+  </svg>
 );
