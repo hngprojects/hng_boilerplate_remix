8844fc2325b38e65c790ebc72fa7394924e52e66
shuaibu-shehu <shuaibushehukalifa@gmail.com>
2024-07-22 00:05:56 +0530

    subscription cancel modal

--- app/components/SubscriptionCancelModal.tsx
+++ app/components/SubscriptionCancelModal.tsx
@@ -1,42 +1,43 @@
-import React from 'react'
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog'
-import { Button } from './ui/button'
-import { useNavigate } from '@remix-run/react'
+import React from 'react';
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog';
+import { Button } from './ui/button';
+import { useNavigate } from '@remix-run/react';
 
 
 interface SubscripptionCancelModalProperties {
-    children: React.ReactNode
-    onReactivateSubscription?: () => void
+  children: React.ReactNode;
+  onReactivateSubscription?: () => void;
 }
 
 
 export default function SubscripptionCancelModal({
-    children,
-    onReactivateSubscription
+  children,
+  onReactivateSubscription
 }: SubscripptionCancelModalProperties) {
-    const navigate= useNavigate();
-  
- const  onGoToDashboard = () => {
-        navigate('/dashboard');
-}
-    return (
+  const navigate = useNavigate();
+
+  const onGoToDashboard = () => {
+    navigate('/dashboard');
+  }
+  return (
     <Dialog>
-    <DialogTrigger asChild>
-      <Button variant="outline">{children}</Button>
-    </DialogTrigger>
-    <DialogContent className="sm:max-w-[425px]">
-      <DialogHeader>
-        <DialogTitle>Subscription canceled</DialogTitle>
-        <DialogDescription>
-         Your subscription has been successfully canceled. If you change your mind, you can reactivate your subscription anytime
-        </DialogDescription>
-      </DialogHeader>
-   
-      <DialogFooter>
-        <Button variant="outline" onClick={onReactivateSubscription}>Reactivate Subscription</Button>
-        <Button type="submit" onClick={onGoToDashboard}>Go to Dashboard</Button>
-      </DialogFooter>
-    </DialogContent>
-  </Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">{children}</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Subscription canceled</DialogTitle>
+          <DialogDescription>
+            Your subscription has been successfully canceled. If you change your mind,
+            you can reactivate your subscription anytime
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter>
+          <Button variant="outline" onClick={onReactivateSubscription}>Reactivate Subscription</Button>
+          <Button type="submit" onClick={onGoToDashboard}>Go to Dashboard</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
