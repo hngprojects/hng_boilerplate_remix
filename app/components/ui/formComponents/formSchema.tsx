bb48d206156fd7e0a41111cd210961a00cb57713
emaicorp <emaicorp@gmail.com>
2024-07-21 17:47:03 +0100

    Remove folder from master branch

--- app/components/ui/formComponents/formSchema.tsx
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-const phoneRegex = new RegExp(/^(\+?[\d\s]+)?(\d{3}|\(?\d+\))?(-?\s?\d)+$/);
-export const FormSchema = z.object({
-  name: z.string().min(2),
-  email: z.string().email(),
-  number: z.string().regex(phoneRegex, "Invalid Number!"),
-});
