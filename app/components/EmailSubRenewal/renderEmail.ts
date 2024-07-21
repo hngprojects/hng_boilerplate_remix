86fce8928f76c881b147f0883ea53d8743561da4
Maryjanechukelu <maryjanechukelu@gmail.com>
2024-07-21 17:28:06 +0100

    prettier reset

--- app/components/EmailSubRenewal/renderEmail.ts
+++ app/components/EmailSubRenewal/renderEmail.ts
@@ -1,7 +1,10 @@
-import { renderToStaticMarkup } from 'react-dom/server';
-import type {Subscription} from '../../components/EmailSubRenewal/EmailRenewal';
-import type { SubscriptionProps } from '../../components/EmailSubRenewal/EmailRenewal'; 
+import { renderToStaticMarkup } from "react-dom/server";
+
+import type {
+  Subscription,
+  SubscriptionProps,
+} from "../../components/EmailSubRenewal/EmailRenewal";
 
 export const renderSubscriptionRenewalEmail = (props: SubscriptionProps) => {
-    return renderToStaticMarkup(<Subscription >{ ...props });
+  return renderToStaticMarkup(<Subscription>{ ...props });
 };
