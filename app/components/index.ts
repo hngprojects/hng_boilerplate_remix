34331f3b817a40d06bf92f0a3bb24c0cddc3d2a9
devasMartins-Tmd <dev.as.martins3@gmail.com>
2024-07-21 19:41:17 +0100

    feat: user dashboard page added

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,1 +1,16 @@
-export {};
+import avatar from "../../public/avatar.png";
+
+
+
+
+
+import avatar from '../../public/avatar.png';
+
+export {  date,      avatar };
+
+export {default as drop} from '../../public/drop.png';
+export {default as icon} from '../../public/icon.png';
+export {default as logo} from '../../public/logo.png';
+export {default as question} from '../../public/question.png';
+export {default as search} from '../../public/search.png';
+export {default as menu} from '../../public/menu.png';
