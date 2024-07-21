5ef3bdbcae2841657c6d65f433bb963476bbcff7
Kwasako <danburamayuba@gmail.com>
2024-07-21 19:40:36 +0100

    feat: move cookieSettings to app/data/ui.ts

--- app/components/ui/CookieConsent/CookieSettings.tsx
+++ app/components/ui/CookieConsent/CookieSettings.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { cookieSettings } from '~/data/ui';
 import {
   Dialog,
   DialogContent,
@@ -73,23 +74,6 @@ const CookieSettings: React.FC = () => {
         setIsClient(true);
     }, []);
 
-    const cookieSettings: CookieSetting[] = [
-        {
-            title: "Performance Cookies",
-            id: "pc",
-            description: "These cookies allow us to count visits and traffic sources so we can measure and improve the performance of our site. They help us to know which pages are the most and least popular and see how visitors move around the site."
-        },
-        {
-            title: "Functional Cookies",
-            id: "fc",
-            description: "These cookies enable the website to provide enhanced functionality and personalisation. They may be set by us or by third party providers whose services we have added to our pages."
-        },
-        {
-            title: "Targeting Cookies",
-            id: "tc",
-            description: "These cookies may be set through our site by our advertising partners. They may be used by those companies to build a profile of your interests and show you relevant adverts on other sites."
-        }
-    ];
 
     const handleToggle = (index: number) => {
         setExpandedIndex(expandedIndex === index ? null : index);
---
+++ app/data/ui.ts
@@ -0,0 +1,17 @@
+export const cookieSettings: CookieSetting[] = [
+    {
+        title: "Performance Cookies",
+        id: "pc",
+        description: "These cookies allow us to count visits and traffic sources so we can measure and improve the performance of our site. They help us to know which pages are the most and least popular and see how visitors move around the site."
+    },
+    {
+        title: "Functional Cookies",
+        id: "fc",
+        description: "These cookies enable the website to provide enhanced functionality and personalisation. They may be set by us or by third party providers whose services we have added to our pages."
+    },
+    {
+        title: "Targeting Cookies",
+        id: "tc",
+        description: "These cookies may be set through our site by our advertising partners. They may be used by those companies to build a profile of your interests and show you relevant adverts on other sites."
+    }
+];
