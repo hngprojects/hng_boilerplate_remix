85d66d87943c93459c7db4cb1571645c95a72953
alialaba <alialaba079@gmail.com>
2024-07-22 12:33:15 -0400

    feat: Invoice Email Template

--- app/root.tsx
+++ app/root.tsx
@@ -6,14 +6,11 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLocation,
 } from "@remix-run/react";
 import type { ReactNode } from "react";
 
-import FooterLight from "./components/ui/footerLight";
-import Header from "./components/ui/header";
+import { AdminSideNavBar } from "./components/SuperAdminSideBar/SuperAdminSideNavBar";
 import styles from "./styles/global.css?url";
-import InvoiceEmail from "./email/templates/invoice-email-temp/InvoiceEmail";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
@@ -21,9 +18,6 @@ export const links: LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: ReactNode }) {
-  const location = useLocation();
-  const pagesWithNoFooter = ["/dashboard/password-settings"];
-  const showFooter = !pagesWithNoFooter.includes(location.pathname);
   return (
     <html lang="en">
       <head>
@@ -33,19 +27,9 @@ export function Layout({ children }: { children: ReactNode }) {
         <Links />
       </head>
       <body>
-
-        <InvoiceEmail/>
         <div className="flex">
-          <main className="flex-1">{children}</main>
-          ,
-
-        <div>
-          <main>
-            <Header />
-            {children}
-            {showFooter && <FooterLight />}
-          </main>
-
+          <AdminSideNavBar />
+          <main className="flex-1">{children}</main>,
           <ScrollRestoration />
           <Scripts />
         </div>
