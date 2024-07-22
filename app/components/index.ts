54f3f6490bf0d928ae0844ffed2811e3385505a6
devasMartins-Tmd <dev.as.martins3@gmail.com>
2024-07-22 17:25:08 +0100

    feat: #41 - resolving conflicting files

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,16 +1,8 @@
-import avatar from "../../public/avatar.png";
+export { default as drop } from "../../public/drop.png";
+export { default as icon } from "../../public/icon.png";
+export { default as logo } from "../../public/logo.png";
+export { default as question } from "../../public/question.png";
+export { default as search } from "../../public/search.png";
+export { default as menu } from "../../public/menu.png";
 
-
-
-
-
-import avatar from '../../public/avatar.png';
-
-export {  date,      avatar };
-
-export {default as drop} from '../../public/drop.png';
-export {default as icon} from '../../public/icon.png';
-export {default as logo} from '../../public/logo.png';
-export {default as question} from '../../public/question.png';
-export {default as search} from '../../public/search.png';
-export {default as menu} from '../../public/menu.png';
+export { default as avatar } from "../../public/avatar.png";
