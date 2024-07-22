f74a5c35f0fd56db9a70810751bc1e63ab170db8
OWK50GA <okoriewilfrid.20201206203@futo.edu.ng>
2024-07-22 06:15:15 +0100

    pnpm test keeps failing because of vitest

--- app/components/ui/accordion.tsx
+++ app/components/ui/accordion.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import * as AccordionPrimitive from '@radix-ui/react-accordion';
+import * as AccordionPrimitive from "@radix-ui/react-accordion";
 import { cn } from "app/lib/utils/cn";
 import { ChevronDown } from "lucide-react";
 import {
