803c17b16e8c1f93450e8bd6f5a17cefe40d5353
OWK50GA <okoriewilfrid.20201206203@futo.edu.ng>
2024-07-22 14:44:36 +0100

    feat: Breadcrumb worked on for external static pages

--- app/routes/pricing.payment.tsx
+++ app/routes/pricing.payment.tsx
@@ -2,12 +2,16 @@ import { useState } from "react";
 
 import DetailsForm from "~/components/DetailsForm";
 import PaymentForm from "~/components/PaymentForm";
+import PricingPaymentBreadCrumb from "~/components/PricingPaymentBreadCrumb";
 
 const Payment = () => {
   const [showSecondForm, setShowSecondForm] = useState(false);
 
   return (
     <div>
+      <div className="mx-auto w-full">
+        <PricingPaymentBreadCrumb />
+      </div>
       <div className="max-w-screen mx-auto flex justify-between gap-20 bg-[#ffffff]">
         <div className="mx-auto flex w-10/12 flex-col gap-6 py-[5%] md:flex-row md:justify-between">
           <div className="w-full">
