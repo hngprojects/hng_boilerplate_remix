28606f9baa6c826993f5dbbae226bd0dfc1a238f
Aded-oyin <danyodele50@gmail.com>
2024-07-22 15:50:51 +0100

    feat: resolve-merge-conflicts

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,8 +10,8 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-      <div className="font-sans p-4">
-        <h1 className="text-3xl">Welcome to Remix</h1>
+    <div className="font-sans p-4">
+      <h1 className="text-3xl">Welcome to Remix</h1>
       <ul className="list-disc mt-4 pl-6 space-y-2">
         <li>
           <a
