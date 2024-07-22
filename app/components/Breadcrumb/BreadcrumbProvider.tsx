59222cf0c3a9d619e98faf05261708b014c073ae
Yimiika <oluwafemiaina8@gmail.com>
2024-07-22 14:33:47 +0100

    Fixed type errors in the BreadcrumbProvider

--- app/components/Breadcrumb/BreadcrumbProvider.tsx
+++ app/components/Breadcrumb/BreadcrumbProvider.tsx
@@ -4,9 +4,9 @@ import Breadcrumb from "./Breadcrumb";
 
 const BreadcrumbProvider: React.FC = () => {
   const location = useLocation();
-  const pathnames = location.pathname.split("/").filter((x) => x);
+  const pathnames = location.pathname.split("/").filter((x: string) => x);
 
-  const breadcrumbPages = pathnames.map((pathname, index) => {
+  const breadcrumbPages = pathnames.map((pathname: string, index: number) => {
     const link = `/${pathnames.slice(0, index + 1).join("/")}`;
     return {
       name: pathname.charAt(0).toUpperCase() + pathname.slice(1),
