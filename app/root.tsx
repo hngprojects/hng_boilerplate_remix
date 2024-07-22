8f0d801a012ff0f1eeb2050a11b7c5943ba5fde3
Caleb <2021.cee.jay@gmail.com>
2024-07-22 01:24:37 +0100

    remove footer in root

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,6 @@ import {
 } from "@remix-run/react";
 import type { ReactNode } from "react";
 
-import FooterLight from "./components/ui/footerLight";
 import styles from "./styles/global.css?url";
 
 export const links: LinksFunction = () => [
@@ -28,10 +27,7 @@ export function Layout({ children }: { children: ReactNode }) {
       </head>
       <body>
         <div className="flex">
-          <main className="flex-1">
-            {children}
-            <FooterLight />
-          </main>
+          <main className="flex-1">{children}</main>
           ,
           <ScrollRestoration />
           <Scripts />
