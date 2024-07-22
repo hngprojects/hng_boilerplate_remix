0cf5bb8110735613e180423136aac3a165fb9f48
OWK50GA <okoriewilfrid.20201206203@futo.edu.ng>
2024-07-22 15:58:16 +0100

    last commit

--- app/routes/pricing.payment.tsx
+++ app/routes/pricing.payment.tsx
@@ -1,17 +1,14 @@
-import { Header } from "@radix-ui/react-accordion";
 import { useState } from "react";
 
 import DetailsForm from "~/components/DetailsForm";
 import PaymentForm from "~/components/PaymentForm";
 import PricingPaymentBreadCrumb from "~/components/PricingPaymentBreadCrumb";
-import Footer from "~/components/ui/footer";
 
 const Payment = () => {
   const [showSecondForm, setShowSecondForm] = useState(false);
 
   return (
     <div>
-      <Header />
       <div className="mx-auto w-full">
         <PricingPaymentBreadCrumb />
       </div>
@@ -19,7 +16,7 @@ const Payment = () => {
         <div className="mx-auto flex w-10/12 flex-col gap-6 py-[5%] md:flex-row md:justify-between">
           <div className="w-full">
             <h2 className="mb-4">Order Details</h2>
-            <div className="w-full rounded bg-[#fff] px-6 py-3">
+            <div className="w-full rounded bg-[#ffffff] px-6 py-3">
               <h2 className="text-lg font-semibold">Basic Plan</h2>
               <div className="flex justify-between leading-loose md:w-full">
                 <span className="leading-loose">Monthly Subscription</span>
