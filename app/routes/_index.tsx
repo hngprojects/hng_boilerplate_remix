2f10c1d25448e1002a269882ff569fe9bf0d5938
Aded-oyin <danyodele50@gmail.com>
2024-07-22 15:39:31 +0100

    feat: resolve-merge-conflicts

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,8 @@ import type { MetaFunction } from "@remix-run/node";
 
 import Success from "~/components/success_page/Success";
 
+import { Button } from "~/components/ui/button";
+
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -9,13 +11,46 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-
 export default function Index() {
 
   return (
 
       <div className="p-4 font-sans">
         <Success />
-      </div>
+        <h1 className="text-3xl">Welcome to Remix</h1>
+      <ul className="list-disc mt-4 pl-6 space-y-2">
+        <li>
+          <a
+            className="text-blue-700 underline visited:text-purple-900"
+            target="_blank"
+            href="https://remix.run/start/quickstart"
+            rel="noreferrer"
+          >
+            5m Quick Start
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-blue-700 underline visited:text-purple-900"
+            target="_blank"
+            href="https://remix.run/start/tutorial"
+            rel="noreferrer"
+          >
+            30m Tutorial
+          </a>
+        </li>
+        <Button>Hello</Button>
+        <li>
+          <a
+            className="text-blue-700 underline visited:text-purple-900"
+            target="_blank"
+            href="https://remix.run/docs"
+            rel="noreferrer"
+          >
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
